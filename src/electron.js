bf1f8798596d9330bb5e322598d538ccf68ab94c
Randy-diebold <randy.diebold@gmail.com>
2021-08-04 18:40:56 -0700

    init commit w/ electon-svelte-typescript boilerplate

---
+++ src/electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // Open Devtools
+  // win.webContents.openDevTools();
+
+  const mode = process.env.NODE_ENV;
+  const url =
+    mode === 'production'
+      ? `file://${path.join(__dirname, '../public/index.html')}`
+      : 'http://localhost:5000';
+
+  win.loadURL(url);
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
