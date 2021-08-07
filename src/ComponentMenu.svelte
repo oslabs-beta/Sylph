a9eb4d423a7a5e0c9117021aa976b007a0c24aec
Kailee Pedersen <kaileepedersen@gmail.com>
2021-08-07 13:50:22 -0400

    horizontal scaling window working, centered components

--- src/ComponentMenu.svelte
+++ src/ComponentMenu.svelte
@@ -13,6 +13,9 @@
 
 <style>
 	section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 		width: 100%;
     height: 100%;
 		padding: 0.3em;
--- src/Sandbox.svelte
+++ src/Sandbox.svelte
@@ -13,7 +13,11 @@
 
 <style>
 	section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 		width: 100%;
+    height: 100%;
 		padding: 0.3em;
 		border: 1px solid black;
 		overflow: scroll;
