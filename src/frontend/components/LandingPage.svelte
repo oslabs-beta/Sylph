ff65b8e1cfa4722510b28ca2334bb9c2e9538b94
Kailee Pedersen <kaileepedersen@gmail.com>
2021-08-23 21:11:19 -0400

    corrected spelling of sylph in main window

---
+++ src/frontend/components/LandingPage.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Router from 'svelte-spa-router'
+  import SylphContainer from '../containers/SylphContainer.svelte'
+
+  const routes = {
+    '/sylph': SylphContainer
+  }
+</script>
+
+<section>
+  <Router {routes} />
+
+  <a href="#/sylph">Start a new Svelte prototyping project</a>
+  <p>[NOT FUNCTIONAL] EXAMPLE 2</p>
+  <p>[NOT FUNCTIONAL] EXAMPLE 3</p>
+</section>
